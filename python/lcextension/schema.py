41ea736721e6b0991dde75f25e6d9427177ffda2
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2023-05-27 14:43:15 -0700

    Schema serialization

--- python/lcextension/schema.py
+++ python/lcextension/schema.py
@@ -7,6 +7,15 @@ class SchemaObject(object):
         self.KeyName = None
         self.Requirements = [] # [] of [] of Field names
 
+    def serialize(self):
+        return {
+            'fields' : {n: f.serialize() for n, f in self.Fields.items()},
+            'render_type' : self.RenderType,
+            'key_data_type' : self.KeyDataType,
+            'key_name' : self.KeyName,
+            'requirements' : self.Requirements,
+        }
+
 class SchemaElement(object):
     def __init__(self):
         self.Label = ""
@@ -20,9 +29,26 @@ class SchemaElement(object):
         self.PlaceHolder = None
         self.SupportedActions = None # [] of Action names
 
+    def serialize(self):
+        return {
+            'label' : self.Label,
+            'description' : self.Description,
+            'data_type' : self.DataType,
+            'is_list' : self.IsList,
+            'display_index' : self.DisplayIndex,
+            'default_value' : self.DefaultValue,
+            'object' : None if self.ObjectSchema is None else self.ObjectSchema.serialize(),
+            'enum_values' : self.EnumValues,
+            'placeholder' : self.PlaceHolder,
+            'supported_actions' : self.SupportedActions,
+        }
+
 class RequestSchemas(object):
     def __init__(self):
         self.Actions = {} # {} of Action name to RequestSchema
+
+    def serialize(self):
+        return {n : a.serialize() for n, a in self.Actions.items()}
 
 class RequestSchema(object):
     def __init__(self):
@@ -33,6 +59,17 @@ class RequestSchema(object):
         self.IsImpersonated = False
         self.ParameterDefinitions = SchemaObject()
         self.ResponseDefinition = None # SchemaObject
+
+    def serialize(self):
+        return {
+            'is_default' : self.IsDefaultRequest,
+            'is_user_facing' : self.IsUserFacing,
+            'short_description' : self.ShortDescription,
+            'long_description' : self.LongDescription,
+            'is_impersonated' : self.IsImpersonated,
+            'parameters' : self.ParameterDefinitions.serialize(),
+            'response' : None if self.ResponseDefinition is None else self.ResponseDefinition.serialize(),
+        }
 
 class SchemaDataTypes(object):
     String = "string"
