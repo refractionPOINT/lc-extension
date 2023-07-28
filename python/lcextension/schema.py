ee0ba09df60239d1520b5a6ea133980b986bba64
Alexander Kim <alexander@refractionpoint.com>
2023-07-28 12:52:51 -0700

    updated schema return

--- python/lcextension/schema.py
+++ python/lcextension/schema.py
@@ -63,6 +63,7 @@ class RequestSchemas(object):
 
 class RequestSchema(object):
     def __init__(self, **kwargs):
+        self.IsDefaultRequest = False
         self.IsUserFacing = True
         self.ShortDescription = ""
         self.LongDescription = ""
@@ -76,6 +77,7 @@ class RequestSchema(object):
 
     def serialize(self):
         return {
+            'is_default' : self.IsDefaultRequest,
             'is_user_facing' : self.IsUserFacing,
             'short_description' : self.ShortDescription,
             'long_description' : self.LongDescription,
