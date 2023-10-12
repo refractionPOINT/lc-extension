9cbfd50ae418eb9c51a35182c8c48917834d69c3
josh-lc <136113933+josh-lc@users.noreply.github.com>
2023-10-12 10:21:17 -0500

    Using secret value logic (#65)
    * Adding using secrets logic
    
    * updating secret name
    
    * Updating using secret value
    
    * Making UsingSecretValue public
    
    * PR reqest changes
    
    * pr request changes

--- core/utils.go
+++ core/utils.go
@@ -1,1 +1,93 @@
 package core
+
+import (
+	"fmt"
+	"github.com/refractionPOINT/go-limacharlie/limacharlie"
+	"path"
+	"strings"
+	"sync"
+)
+
+var (
+	secretCache = make(map[string]string)
+	cacheMutex  sync.RWMutex
+)
+
+func UseSecretValue(key string, org *limacharlie.Organization, fn func(val string) error) error {
+	var err error
+	var apiKey string
+	var exists bool
+	if strings.Contains(key, "hive://secret/") {
+		secretName := path.Base(key)
+		for i := 0; i < 2; i++ { // max of two tries
+			// Try to get secret from cache
+			apiKey, exists = getSecretFromCache(secretName)
+			if !exists {
+				apiKey, err = getSecretFromHive(secretName, org)
+				if err != nil {
+					return err
+				}
+
+				// ensure to update local cache
+				setSecretCache(secretName, apiKey)
+			}
+
+			err = fn(apiKey)
+			if err == nil {
+				return nil
+			}
+
+			if i == 1 { // no need to clear cache this is 2nd call
+				return err
+			}
+
+			// Clear the cache to ensure the next iteration fetches the secret from Hive again
+			deleteSecretCache(secretName)
+
+		}
+		return fmt.Errorf("secrets function failed for org: %s err: %v", org.GetOID(), err)
+	}
+
+	// no retry logic if actual key passed
+	if err := fn(apiKey); err != nil {
+		return err
+	}
+	return nil
+}
+
+func setSecretCache(secretName, apiKey string) {
+	cacheMutex.Lock()
+	secretCache[secretName] = apiKey
+	cacheMutex.Unlock()
+}
+
+func getSecretFromCache(secretName string) (string, bool) {
+	cacheMutex.RLock()
+	val, exists := secretCache[secretName]
+	cacheMutex.RUnlock()
+	return val, exists
+}
+
+func deleteSecretCache(secretName string) {
+	cacheMutex.Lock()
+	delete(secretCache, secretName)
+	cacheMutex.Unlock()
+}
+
+func getSecretFromHive(recordName string, org *limacharlie.Organization) (string, error) {
+	hc := limacharlie.NewHiveClient(org)
+	data, err := hc.Get(limacharlie.HiveArgs{
+		HiveName:     "secret",
+		PartitionKey: org.GetOID(),
+		Key:          recordName,
+	})
+	if err != nil {
+		return "", err
+	}
+	value, ok := data.Data["secret"].(string)
+	if !ok || value == "" {
+		return "", fmt.Errorf("secret not set or is not of type string")
+	}
+
+	return data.Data["secret"].(string), nil
+}
