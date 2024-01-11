56fe4dc0d3c8d21774e1eae8caa367ba309c6c5d
josh-lc <136113933+josh-lc@users.noreply.github.com>
2024-01-11 15:50:46 -0600

    Webhook adapter-python (#89)
    * start of webhood adapter logic calls converted from golang
    
    * added crea nd dele extension and fixed some code
    
    * updating raised exceptions now not throwing error on record not found and fixed hive record in create ext adapter
    
    * removing comments
    
    * fixing spacing
    
    * added lock to beginning of call in get adapter client and removed secondary lock call

--- python/lcextension/ext.py
+++ python/lcextension/ext.py
@@ -27,6 +27,9 @@ class Extension(object):
 
         self._app = flask.Flask(self._name)
 
+        self.wh_clients = {}  # equivalent to whClients in Go
+        self.wh_clients_lock = threading.Lock()  # For thread-safe access to wh_clients
+
         @self._app.post('/')
         def _handler():
             sig = flask.request.headers.get('lc-ext-sig', None)
@@ -159,3 +162,123 @@ class Extension(object):
             } ) )
             sys.stderr.write( "\n" )
             sys.stderr.flush()
+
+    ####### start of webhook methods #####
+            
+    def create_extension_adapter(self, manager, opt_mapping={}):
+        private_tag = self.get_extension_private_tag()
+        try:
+            response = manager.create_installation_key(["lc:system", private_tag], self.get_extension_adapter_installation_key_desc())
+        except Exception as e:
+            raise limacharlie.LcApiException(f"failed to create installation key : {e}")
+
+        hive = limacharlie.Hive(manager, "cloud_sensor", manager._oid)
+        iid = response["iid"]
+        data = {
+                "usr_mtd":{
+                    "enabled": True,
+                    "tags": ["lc:system", private_tag],
+                },
+                "data": {
+                    "sensor_type": "webhook",
+                    "webhook": {
+                        "secret": self.generate_webhook_secret_for_org(manager._oid),
+                        "client_options": {
+                            "hostname": self._name, # ext-name
+                            "identity": {
+                                "oid": manager._oid,
+                                "installation_key": iid,
+                            },
+                            "platform": "json",
+                            "sensor_seed_key": self._name, # ext-name
+                            "mapping": opt_mapping,
+                        },
+                    },
+                },
+            }
+        
+        try: 
+            hive_record = limacharlie.HiveRecord(self._name, data)
+            hive.set(hive_record)
+        except Exception as e:
+            raise limacharlie.LcApiException(f"failed to create webhook adapter: {e}")
+
+    def delete_extension_adapter(self, manager):
+        hive = limacharlie.Hive(manager, "cloud_sensor", manager._oid)
+        try:
+            hive.delete(self._name)
+        except Exception as e:
+                if not (isinstance(e, limacharlie.LcApiException) and "RECORD_NOT_FOUND" in str(e)):
+                    raise limacharlie.LcApiException(f"failed hive delete: {e}")
+        
+        install_key_desc = self.get_extension_adapter_installation_key_desc()
+        try:
+            install_keys = manager.get_installation_keys()
+        except Exception as e:
+            raise limacharlie.LcApiException(f"failed to list installation keys: {e}")
+        
+        private_tag = self.get_extension_private_tag()
+
+        for org_id, keys in install_keys.items():
+            for key_id, key in keys.items():
+                if key['desc'] != install_key_desc:
+                    continue
+
+                tags = [tag.strip() for tag in key['tags'].split(',') if tag.strip()]
+                is_tag_found = private_tag in tags
+                if not is_tag_found:
+                    continue
+
+                try:
+                    manager.delete_installation_key(key['iid'])
+                except Exception as e:
+                    if not (isinstance(e, limacharlie.LcApiException) and "RECORD_NOT_FOUND" in str(e)):
+                        raise limacharlie.LcApiException(f"Failed to delete installation key {key['iid']: [e]}")
+
+
+    def generate_webhook_secret_for_org(self, oid):
+        # This generates a secret value deterministically from
+        # the OID so that we can easily know the webhook to
+        # hit without having to query LC. The WEBHOOK_SECRET
+        # needs to remain secret to avoid someone possibly
+        # sending their own data to users.
+        h = hashlib.sha256()
+        h.update(self._secret.encode())
+        h.update(oid.encode())
+        return h.hexdigest()
+    
+    def get_extension_adapter_installation_key_desc(self):
+        # Returns a description string for the extension adapter installation key
+        return f"ext {self._name} webhook adapter"
+
+    def get_adapter_client(self, manager):
+        # try to get the client if it already exists
+        with self.wh_clients_lock:
+            client = self.wh_clients.get(manager._oid)
+
+            if client:
+                return client
+
+            # Create a new client if it doesn't exist
+            try:
+                new_client = limacharlie.WebhookSender(manager, self._name, self.generate_webhook_secret_for_org(manager._oid))
+            except Exception as e:
+                raise Exception(f"failed to create webhook sender client: {e} ")
+
+            self.wh_clients[manager._oid] = new_client
+
+            return new_client
+    
+    def send_to_webhook_adapter(self, manager, data):
+        try:
+            wh_client, error = self.get_adapter_client(manager._oid)
+        except Exception as e:
+            raise Exception(f"failed to get adapter client: {e}")
+        
+        try:
+            wh_client.send(data)
+        except Exception as e:
+            raise Exception(f"failed webhook client send: {e}")
+    
+    def get_extension_private_tag(self):
+        return f"ext:{self._name}"
