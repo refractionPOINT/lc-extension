13ea577deb541887e44437d36b481df07ee45c15
Adele Noronha <adele@refractionpoint.com>
2024-03-25 14:32:52 -0700

    extend log line for test

--- simplified/dr.go
+++ simplified/dr.go
@@ -150,7 +150,7 @@ func (l *RuleExtension) Init() (*core.Extension, error) {
 					l.Logger.Error(fmt.Sprintf("failed to add scheduling D&R rule: %s", err.Error()))
 					return common.Response{Error: err.Error()}
 				}
-				l.Logger.Info("successfully inside the debug-log branch")
+				l.Logger.Info("successfully inside the debug-log branch!!")
 				l.Logger.Info(fmt.Sprintf("%+v /n", l.EventHandlers))
 				if h, ok := l.EventHandlers[common.EventTypes.Subscribe]; ok {
 					l.Logger.Info("found EventHandler for Subscribe event, callback fired")
