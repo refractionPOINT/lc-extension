ed56e6c28014ead9c7811a89f60d664eb23ffa58
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2024-12-09 17:12:46 -0800

    Example rule.

---
+++ python/examples/panther/rules/slack_app_access_expanded.py
@@ -0,0 +1,50 @@
+# Taken from: https://github.com/panther-labs/panther-analysis/blob/develop/rules/slack_rules/slack_app_access_expanded.py
+# from panther_slack_helpers import slack_alert_context
+
+ACCESS_EXPANDED_ACTIONS = [
+    "app_scopes_expanded",
+    "app_resources_added",
+    "app_resources_granted",
+    "bot_token_upgraded",
+]
+
+
+def rule(event):
+    return event.get("action") in ACCESS_EXPANDED_ACTIONS
+
+
+def title(event):
+    return (
+        f"Slack App [{event.deep_get('entity', 'app', 'name')}] "
+        f"Access Expanded by [{event.deep_get('actor', 'user', 'name')}]"
+    )
+
+
+def alert_context(event):
+    # context = slack_alert_context(event)
+    context = {}
+
+    # Diff previous and new scopes
+    new_scopes = event.deep_get("details", "new_scopes", default=[])
+    prv_scopes = event.deep_get("details", "previous_scopes", default=[])
+
+    context["scopes_added"] = [x for x in new_scopes if x not in prv_scopes]
+    context["scoped_removed"] = [x for x in prv_scopes if x not in new_scopes]
+
+    return context
+
+
+def severity(event):
+    # Used to escalate to High/Critical if the app is granted admin privileges
+    # May want to escalate to "Critical" depending on security posture
+    if "admin" in event.deep_get("entity", "app", "scopes", default=[]):
+        return "High"
+
+    # Fallback method in case the admin scope is not directly mentioned in entity for whatever
+    if "admin" in event.deep_get("details", "new_scope", default=[]):
+        return "High"
+
+    if "admin" in event.deep_get("details", "bot_scopes", default=[]):
+        return "High"
+
+    return "Medium"
